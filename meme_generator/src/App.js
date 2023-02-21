3fa4b18ae1d516eea66330ef41d04e8ff3f7870b
arpanmalik <arpanmalik2000@gmail.com>
2023-02-21 23:58:26 +0530

    added

---
+++ meme_generator/src/App.js
@@ -0,0 +1,16 @@
+import logo from './logo.svg';
+import './App.css';
+
+import React from "react"
+import Header from "./Header"
+import Meme from "./Meme"
+
+
+export default function App() {
+    return (
+        <div>
+            <Header />
+            <Meme />
+        </div>
+    )
+}
